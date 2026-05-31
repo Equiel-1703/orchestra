faf7a610c4e34bdb74ce1ae01118cead0e28bd64
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-05-31 03:31:20 -0300

    Add pure Elixir implementation for dot product benchmark

---
+++ benchmarks/cpu/dot_product_elixir.exs
@@ -0,0 +1,18 @@
+[arg] = System.argv()
+
+size = String.to_integer(arg)
+
+list_a = List.duplicate(1, size)
+list_b = List.duplicate(2, size)
+
+prev = System.monotonic_time()
+
+res = Enum.zip_reduce(list_a, list_b, 0, fn a, b, acc ->
+  acc + (a * b)
+end)
+
+next = System.monotonic_time()
+
+IO.inspect(res, label: "Dot product result")
+IO.puts("Expected result: #{size * 2}")
+IO.puts("Elixir\t#{size}\t#{System.convert_time_unit(next - prev, :native, :millisecond)}")
