6d3c046b74a38d54f28de000f58e77c2a086cbb2
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-05-31 02:10:26 -0300

    Test with pure Nx

---
+++ benchmarks/cpu/mm_nx_2.exs
@@ -0,0 +1,26 @@
+arg =
+  try do
+    [arg] = System.argv()
+    arg
+  rescue
+    _ ->
+      IO.puts("Usage: mix run benchmarks/mm.ex [MATRIX_SIZE]")
+      IO.puts("  - MATRIX_SIZE: Size of the square matrices to be multiplied (MxM)")
+      System.halt(0)
+  end
+
+size = String.to_integer(arg)
+
+matrix_a = Nx.broadcast(1, {size, size})
+matrix_b = Nx.broadcast(2, {size, size})
+
+timing_start = System.monotonic_time()
+
+_result = Nx.dot(matrix_a, matrix_b)
+
+timing_end = System.monotonic_time()
+
+# Calculate times in milliseconds
+time = System.convert_time_unit(timing_end - timing_start, :native, :millisecond)
+
+IO.puts("Nx.dot\t#{size}\t#{time}")
